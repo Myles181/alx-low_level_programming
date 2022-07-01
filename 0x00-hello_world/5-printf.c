1f7bd7e831e0cc33f5c925f997a99d39fb844e80
Myles2005 <cyrile450@gmail.com>
2022-07-01 12:57:14 -0700

    use print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	 /*use printf instead of puts*/
+	/* return 0 to success*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
