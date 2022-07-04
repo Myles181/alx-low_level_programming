c5cfd117c42c04a058fd49e495d9af6fd5e6fe47
Myles2005 <cyrile450@gmail.com>
2022-07-04 09:39:23 -0700

    #include <stdio.h>

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry main
+ *
+ * Description: Use of putchar print lower_case and upper_case
+ *
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	for (CH = 'A'; CH <= 'Z'; ch++)
+	putchar(ch);
+	putchar(CH);
+
+	return (0);
+}
