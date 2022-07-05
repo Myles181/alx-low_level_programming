e4ef6826b6b709735a5b18efae00f810cd70d7bb
Myles2005 <cyrile450@gmail.com>
2022-07-04 18:59:47 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Number from 0 to 9
+ *
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	return (0);
+}
