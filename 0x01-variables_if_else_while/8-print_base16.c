f2bf820e24364084d81b3b3102d548a7db1de7ee
Myles2005 <cyrile450@gmail.com>
2022-07-05 02:55:30 -0700

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print lower case numbers of base 16 with putchar
+ *
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	char ch = '0';
+	char ch1 = 'a';
+	while (ch < '10');
+	{
+	putchar(ch);
+	ch++;
+	}
+	while (ch1 < 'g');
+	{
+		putchar(ch1);
+		ch1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
