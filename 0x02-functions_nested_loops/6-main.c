09b0eb98551a19204d885b01ee4cf564ced0d989
Myles2005 <cyrile450@gmail.com>
2022-07-06 04:01:51 -0700

    commit

--- 0x02-functions_nested_loops/6-main.c
+++ 0x02-functions_nested_loops/6-main.c
@@ -1,32 +1,33 @@
 #include "main.h"
-#include <stdio.h>
 
+#include <stdio.h>
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    int r;
-    r = 0;
 
-   if ( r == -1)
-   {
-    printf("%d\n", r);
-   }
-    if (r == 0)
-    {
-    printf("%d\n", r);
-    }
-    if (r == 1)
-    {
-    printf("%d\n", r);
-    }
-   if (r == -98)
-   {
-    printf("%d\n", r);
-   }
-    
-   return (0);
+int main(void)
+
+{
+	int r;
+
+	r = _abs(-1);
+
+	printf("%d\n", r);
+
+	r = _abs(0);
+
+	printf("%d\n", r);
+
+	r = _abs(1);
+
+	printf("%d\n", r);
+
+	r = _abs(-98);
+
+	printf("%d\n", r);
+
+	return (0);
+
 }
