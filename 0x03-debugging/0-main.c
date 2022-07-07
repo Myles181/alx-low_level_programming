71ea4cfabeac0fbccbeeb635d3535ba98a4e31fe
Myles2005 <cyrile450@gmail.com>
2022-07-07 15:33:22 -0700

    commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,13 +2,13 @@
 
 /**
  * main - tests function that print if integer is positive or negative
- * Returns: 0
-*/
+ * Return: 0
+ */
 
 int main(void)
 {
 	int i;
-	
+
 	i = 98;
 	positive_or_negative(i);
 
