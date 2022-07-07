ff2e74ccc96781cee81be80527407fe66fc41ed4
Myles2005 <cyrile450@gmail.com>
2022-07-07 11:27:01 -0700

    correction

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,28 +7,22 @@
  *
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	a = 972;
-	b = -98;
-	c = 0;
-
 	if (a > c && c > b)
 	{
 		largest = a;
-		printf("%d", largest);
 	}
 	else if (b > c && c > a)
 	{
 		largest = b;
-		printf("%d", largest);
 	}
 	else
 	{
 		largest = c;
-		printf("%d", largest);
 	}
-	return (0);
+	return (largest);
 }
