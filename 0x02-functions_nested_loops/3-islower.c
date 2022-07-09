f10d2eaa3678cfa9022df1506928f556754e20bd
Myles2005 <cyrile450@gmail.com>
2022-07-08 18:19:02 -0700

    changes

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - Entry point
- *
+ *@c: the variable
  * Descripption: show if 'c' is a lower_case letter
  * return 1 if true and 0 if false
  *
@@ -9,9 +9,8 @@
  */
 int _islower(int c)
 {
-	int ch = c;
 
-	if (ch >= 97 && ch <= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
