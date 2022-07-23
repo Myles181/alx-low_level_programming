9f571c36f28bcba49b406aa6f8400667c59251a6
Myles2005 <cyrile450@gmail.com>
2022-07-22 21:02:15 -0700

    print out compiler name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print out string 0.
+ * @argc: Count argument
+ * @argv: Vector argument
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
