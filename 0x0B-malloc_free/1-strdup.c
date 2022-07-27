3bad4d31329ff45191e7294c1a50223d187e38f1
Myles2005 <cyrile450@gmail.com>
2022-07-27 13:42:10 -0700

    copy string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - To copy a string
+ * @str: first pamam
+ * Return: c
+ */
+char *_strdup(char *str)
+{
+	int i, size = 0;
+	char *copy;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[size] < '\0')
+	{
+		size++;
+	}
+
+	copy = (char *)malloc((sizeof(char) * size) + 1);
+
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i < size + 1; size++)
+	{
+		copy[i] = str[i];
+	}
+	copy[size] = '\0';
+	return (copy);
+}
