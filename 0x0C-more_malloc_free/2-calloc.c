635acc442e94aac3b207ab265e319914b1ca7502
Myles2005 <cyrile450@gmail.com>
2022-07-29 13:21:00 -0700

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,19 +10,21 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
 	char *p;
+	void *mem;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb * size);
+	mem = malloc(nmemb * size);
+	p = mem;
 	if (p  == NULL)
 		return (NULL);
 
 	while (i < (nmemb * size))
 	{
-		p[i] = 1;
+		p[i] = size++;
 		i++;
 	}
 	p[i] = '\0';
-	return (p);
+	return (mem);
 }
