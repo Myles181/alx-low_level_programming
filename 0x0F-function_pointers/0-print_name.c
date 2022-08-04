db3320f114c8c37f13856b58a97d5815f2ffe4fd
Myles2005 <cyrile450@gmail.com>
2022-08-03 17:40:58 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
-/** print_name - To print the name of a string
+/**
+ * print_name - To print the name of a string
  * @name: This is the string parameter
+ * @f: Function pointer
  * Return: Always 0.
  */
 void print_name(char *name, void (*f)(char *))
