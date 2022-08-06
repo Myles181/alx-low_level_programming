fa47bb12e30fec0989a25ba4f5db72826328f986
Myles2005 <cyrile450@gmail.com>
2022-08-06 10:47:03 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - dog class
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+
+
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
