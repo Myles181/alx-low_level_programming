857a7d277cbdcb1114805a127def75f6da35a61a
Myles2005 <cyrile450@gmail.com>
2022-08-09 09:03:30 -0700

    update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,22 @@
 #include "main.h"
+#include <string.h>
 /**
  * rev_string - Entry point
  * @s: Write letters backward
  */
 void rev_string(char *s)
 {
+	int len, i = 0;
+	char str;
 
+	len = strlen(s);
+
+	for (; i < len / 2; i++)
+	{
+		str = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = str;
+	}
+	_putchar(*s);
+	_putchar('\n');
 }
