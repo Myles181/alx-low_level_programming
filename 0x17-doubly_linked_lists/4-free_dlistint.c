027ac560454ef60d48ebb43408275493951922d4
ogheneCodes <cyrile450@gmail.com>
2022-09-30 10:19:46 -0700

    free the list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_dlistint - free function
+ * @head: head pointer
+ * Return: void
+ */
+ void free_dlistint(dlistint_t *head)
+{
+	dlistint *ptr;
+
+	ptr = head;
+	while (ptr != NULL)
+	{
+		head = ptr->next;
+		free(ptr);
+		ptr = ptr->next;
+	}
+	free(head);
+}
