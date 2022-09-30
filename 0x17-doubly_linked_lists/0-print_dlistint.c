27f2b483d7425e919819f5b671aa004cb0fc0dba
ogheneCodes <cyrile450@gmail.com>
2022-09-29 18:10:22 -0700

    print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_dlistint - prints linked lists
+ * h: head of the list
+ * Return: i
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *ptr;
+	int i = 0;
+
+	ptr = h;
+
+	while(ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		i++;
+	}
+	return (i);
+}
