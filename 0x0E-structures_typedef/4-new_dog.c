e06588c0e15ed4d7ffab8b8c14961449f0ef97fd
ogheneCodes <cyrile450@gmail.com>
2022-10-30 15:02:07 -0700

    revisiting tasks

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *my_dog;
+
+	my_dog = malloc(sizeof(dog_t));
+
+	if (name == NULL || owner == NULL)
+	{
+		return NULL;
+	}
+
+	my_dog->name = name;
+	my_dog->owner = owner;
+	my_dog->age = age;
+
+	return (my_dog);
+}
