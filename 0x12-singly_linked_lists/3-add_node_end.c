c027e0cb54049ebbefbc091354c444f6a9549d63
ogheneCodes <cyrile450@gmail.com>
2022-10-30 17:42:46 -0700

    node at end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -23,6 +23,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	new_node->len = strlen(new_node->str);
 	new_node->next = NULL;
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
 	if (*head == NULL)
 	{
