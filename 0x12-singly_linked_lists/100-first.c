e3ebbd6079110bb16f12fd6deec1d97fb9acabc3
ogheneCodes <cyrile450@gmail.com>
2022-10-30 17:23:27 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,13 @@
 
 void __attribute__((constructor)) before_main();
 
+/**
+ * before_main - prints out the message before the main function
+ *
+ * Return: nothing
+ */
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
