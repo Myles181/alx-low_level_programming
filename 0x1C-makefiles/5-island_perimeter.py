a262384658df097110105ecd891f8e82f19af563
Myles2005 <cyrile450@gmail.com>
2023-02-26 17:17:36 -0800

    5-island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,10 +5,28 @@ This code is to find the perimeter of a grid
 
 def island_perimeter(grid):
     """Description:
+        Finds the perimeter of a grid
+        1 represent land
+        0 represent water
     Args:
-        grid - The 2d grid
+        grid(list)
+
+    Return:
+        perimeter
     """
-    for j in grid:
-        perimeter = len(j)
-    perimeter *= 2
+    perimeter = 0
+    side = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                side = 4
+                if grid[i - 1][j] == 1:
+                    side -= 1
+                if grid[i + 1][j] == 1:
+                    side -= 1
+                if grid[i][j - 1] == 1:
+                    side -= 1
+                if grid[i][j + 1] == 1:
+                    side -= 1
+                perimeter += side
     return perimeter
