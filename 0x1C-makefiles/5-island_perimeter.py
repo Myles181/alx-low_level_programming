fbb69673ff02f21012e05c8fa0d6cd28575e89eb
Myles2005 <cyrile450@gmail.com>
2023-02-26 16:47:31 -0800

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,10 @@ This code is to find the perimeter of a grid
 """
 
 def island_perimeter(grid):
+    """Description:
+    Args:
+        grid - The 2d grid
+    """
     for j in grid:
         perimeter = len(j)
     perimeter *= 2
